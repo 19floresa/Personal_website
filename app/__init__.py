46c4d49515f7412514dc9fd8d662cd71bc1641be
19floresa <alexander.flores0920@gmail.com>
2022-12-31 23:32:36 -0800

    Users can register and login(required to access website)
    In the file forms.py, a new class for buttons is created for user registration.
    
    In the file register.html, the webpage template for registration is created.
    
    In the file models.py, the User class is modified (new method) so the class can
    set passwords and check them. Including to this, the User class extends UserMixin
    so it can have access to is_authenticated(), is_active(), is_anonymous(), and
    get_id(). Finaly, a new method to load user is created.
    
    In the file routes.py, the login() method is modified so it takes advantage
    of flask_login methods. A new method for logout() is created to log out users.
    These methods are created so a login is required when accessing my website (
    and also to take advatage the decorator @login_required)
    
    Finaly, the file register.html is created for the registration web page.

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_login import LoginManager
 # flask instance (__name__is the current module (maybe; 90% sure))
 app = Flask(__name__)
 # configure app with Config class
@@ -11,6 +11,10 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 # migration engine
 migrate = Migrate(app, db)
+# user login state manager
+login = LoginManager(app)
+# tell the manager what func logs in user so it can use @login_requried
+login.login_view = 'login'
 
 # from app directory
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,16 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 # return render_template("<file_name.html>", "<title="<something>">", ...)
-#Note: title can be any variable you defined in the html file
-from app import app
-from app.forms import LoginForm
+# Note: title can be any variable you defined in the html file
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {"username": "Alex"}
     posts = [
             {
             
@@ -24,22 +27,59 @@ def index():
             ]
     
     # render the index web page
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    # check if user is already logged in
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
     # login class created in  a differed file
     form = LoginForm()
     
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        # look for the user
+        user = User.query.filter_by(username=form.username.data).first()
+        
+        # flash error if no user or incorrect password
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        
+        # registed the user as logged in for that user
+        login_user(user, remember=form.remember_me.data)
+
+        # redirect back if succesful login
+        next_page = request.args.get('next')
+        # no next arg or domain outside website then redirect to index
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+
         # user logged in
-        return redirect(url_for('index'))
+        return redirect(next_page)
 
     # render the login webpage
     return render_template('login.html', title='Sign In', form=form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
 
